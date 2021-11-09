62356a499bbf0f30233c62a42af8c76ed83816fb
Maxim <xbultazar@gmail.com>
2021-11-09 17:18:45 +0300

    transferred add new cars into constructor

--- src/main/java/web/dao/CarDAOImpl.java
+++ src/main/java/web/dao/CarDAOImpl.java
@@ -1,7 +1,6 @@
 package web.dao;
 
 import org.springframework.stereotype.Repository;
-import org.springframework.stereotype.Service;
 import web.model.Car;
 
 import java.util.ArrayList;
@@ -9,17 +8,19 @@ import java.util.List;
 
 @Repository
 public class CarDAOImpl implements CarDAO {
+    private List<Car> cars = new ArrayList<>();
 
-    List<Car> cars = new ArrayList<>();
-
-    @Override
-    public List<Car> getCars(int index) {
-
+    public CarDAOImpl(List<Car> cars) {
+        this.cars = cars;
         cars.add(new Car("BMW", "black", 7));
         cars.add(new Car("Mercedes Benz", "white", 600));
         cars.add(new Car("Volkswagen", "red", 5));
         cars.add(new Car("Audi", "blue", 80));
         cars.add(new Car("Porsche", "purple", 777));
+    }
+
+    @Override
+    public List<Car> getCars(int index) {
 
         if (index <= 5 && index > 0) {
             return cars.subList(0, index);
